aa25d8f91c5689c4cb09fff941b44cb37ea411ff
binzecao <binzecao@sina.com>
2013-09-05 13:09:44 +0800

    V0.0.4-增加创建数据库

--- simple_chat/mysql.sql
+++ simple_chat/mysql.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE simple_chat DEFAULT CHARACTER SET utf8; 
+
+USE simple_chat;
+
 CREATE TABLE dialog (
   id int(11) NOT NULL auto_increment,
   txt text,
@@ -5,4 +9,4 @@ CREATE TABLE dialog (
   date datetime default NULL,
   clientIP varchar(15) default NULL,
   PRIMARY KEY  (id)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='To Recode Dialogs'
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='To Recode Dialogs';
